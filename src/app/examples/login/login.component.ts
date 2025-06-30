4a16e2a1ea9a0d6e66ddac5ce2fbaf0dfaefcca5
Alex Collareda <alex.collareda@hotmail.com>
2025-06-30 17:14:51 -0400

    atualizacao do site

--- src/app/examples/login/login.component.ts
+++ src/app/examples/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +12,10 @@ export class LoginComponent implements OnInit {
     data : Date = new Date();
     focus;
     focus1;
+      cnpj: string = '';
+  password: string = '';
 
-    constructor() { }
+    constructor(private http: HttpClient, private router: Router) {}
 
     ngOnInit() {
         var body = document.getElementsByTagName('body')[0];
@@ -27,5 +31,26 @@ export class LoginComponent implements OnInit {
         var navbar = document.getElementsByTagName('nav')[0];
         navbar.classList.remove('navbar-transparent');
     }
+
+    login() {
+    const payload = {
+      cnpj: this.cnpj,
+      password: this.password
+    };
+
+    this.http.post<any>('http://localhost:8082/api/login-company/login', payload)
+      .subscribe({
+        next: (response) => {
+          // Aqui você pode salvar o token ou dados do usuário, se o backend retornar
+          // Exemplo: localStorage.setItem('token', response.token);
+          // Redireciona para a área logada
+           alert('Deu certo!');
+          this.router.navigate(['/dashboard']);
+        },
+        error: () => {
+          alert('CNPJ ou senha inválidos!');
+        }
+      });
+  }
 
 }
