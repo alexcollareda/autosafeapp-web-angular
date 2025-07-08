9d227f8be54ea6bee184a7b685806e46e635840a
Collaredaa <gustavo.collareda.oliveira@gmail.com>
2025-07-08 20:33:47 -0300

    KAN-30
    foi feito as telas de alteração de senha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,29 +14,32 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 import { LoggedInLayoutComponent } from './layouts/logged-in-layout/logged-in-layout.component';
 import { NavbarLoggedComponent } from './shared/navbar-logged/navbar-logged.component';
-
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        DashboardComponent,
-        PublicLayoutComponent,
-        LoggedInLayoutComponent,
-        NavbarLoggedComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        NgbModule,
-        FormsModule,
-        RouterModule,
-        AppRoutingModule,
-        ComponentsModule,
-        ExamplesModule,
-        HttpClientModule,
-        NgxMaskModule.forRoot()
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    PublicLayoutComponent,
+    LoggedInLayoutComponent,
+    NavbarLoggedComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    ComponentsModule,
+    ExamplesModule,
+    HttpClientModule,
+    NgxMaskModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,6 +13,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './services/auth.guard';
 import { LoggedInLayoutComponent } from './layouts/logged-in-layout/logged-in-layout.component';
 import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { RecoverPasswordComponent } from './examples/recover-password/recover-password.component';
 
 const routes: Routes = [
   // Rotas para a área pública (sem autenticação)
@@ -27,6 +28,7 @@ const routes: Routes = [
         { path: 'examples/login',       component: LoginComponent },
         { path: 'examples/profile',     component: ProfileComponent },
         { path: 'examples/register',    component: RegisterComponent },
+        { path: 'examples/recover-password',    component: RecoverPasswordComponent },
     ]
   },
 
