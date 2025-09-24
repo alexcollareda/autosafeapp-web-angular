d9acf1b3734adbdc37040845355e56ae19464581
Collaredaa <gustavo.collareda.oliveira@gmail.com>
2025-09-24 13:48:10 -0300

    Foi adicionado a parte de informative e a funçao se nao tiver horario levar para tela de my company e direto para o horario

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/services/auth.service';
 import { ServicesService } from 'app/services/services.service';
 import { MetricsService } from 'app/services/metrics.service';
+import { OperatingHoursService } from 'app/services/operating-hours.service';
+import { Router } from '@angular/router';
 
+export interface Informative {
+  message: string;
+  action?: () => void;
+}
 export interface Review{
   rate:number;
 }
@@ -22,8 +28,10 @@ export class DashboardComponent implements OnInit {
     averageRate: number = 0;
     callCount: number = 0;
     viewCount: number = 0;
+    informatives: Informative[] = [];
 
-    constructor(private authService: AuthService, private servicesService: ServicesService, private reviewService: ReviewService, private metricsService:MetricsService) {   
+    constructor(private authService: AuthService, private servicesService: ServicesService, private reviewService: ReviewService, private metricsService:MetricsService, private operatingHoursService: OperatingHoursService,
+    private router: Router) {   
     }
 
   ngOnInit(): void {
@@ -55,7 +63,27 @@ export class DashboardComponent implements OnInit {
         console.error('Erro ao buscar reviews:', error);
       } 
     );
+    this.operatingHoursService.getOperatingHours().subscribe({
+  next: (data) => {
+    if (!data || data.length === 0) {
+      this.informatives.push({
+        message: "Você não possui horário de funcionamento cadastrado, Atualize!",
+        action: () => this.goToMyCompanyTab('horario')
+      });
+    }
+  },
+  error: (err) => {
+    console.error('Erro ao buscar horários de funcionamento:', err);
+    // Se o backend deu 500, adiciona o mesmo informativo
+    this.informatives.push({
+      message: "Você não possui horário de funcionamento cadastrado, Atualize!",
+      action: () => this.goToMyCompanyTab('horario')
+    });
+  }
+});
   }
 
-
+  goToMyCompanyTab(tab: string) {
+  this.router.navigate(['/app/my-company'], { queryParams: { tab } });
+}
 }
